6f663ab322d5e46853d0ca358232958d4a6d709d
John Pittman Gibson <117667506+Rabidalton@users.noreply.github.com>
2023-07-09 01:35:44 +0000

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-var numOfDrums = document.querySelectorAll(".drum").length;
-for(var i=0; i<numOfDrums; i++){
-	document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-	alert("You hit the drum!");
-		});
-}
-
